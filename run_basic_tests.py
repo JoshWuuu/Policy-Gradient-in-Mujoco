4c577c41503c0821a789503d7c488edf46a3c766
ChunLiang Wu <lucky0978218035@gmail.com>
2023-03-21 00:14:04 -0700

    add result

--- run_basic_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import sys, os
-sys.path.append(os.path.join(sys.path[0],'code'))
-
-if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(suite)
